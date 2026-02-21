0b4120a16919691455b69985fde1884dc175de30
Madhusudan Bhandari <madhusudanb636@gmail.com>
2026-02-21 21:22:12 +0545

    admin dashboard update

--- flutter_application_1/lib/core/services/route_services.dart
+++ flutter_application_1/lib/core/services/route_services.dart
@@ -4,6 +4,15 @@ import 'package:http/http.dart' as http;
 class RouteServices {
   static const baseUrl = "http://localhost:5000/api/route";
 
+  static Future<bool> addRoute(Map<String, dynamic> data) async {
+    final res = await http.post(
+      Uri.parse("$baseUrl/routes/add"),
+      headers: {"Content-Type": "application/json"},
+      body: jsonEncode(data),
+    );
+    return res.statusCode == 201;
+  }
+
   static Future<List<dynamic>> getRoutes() async {
     final res = await http.get(Uri.parse(baseUrl));
     if (res.statusCode == 200) {
@@ -11,5 +20,14 @@ class RouteServices {
     } else {
       return [];
     }
+  }
+
+  static Future<bool> addStop(Map<String, dynamic> data) async {
+    final res = await http.post(
+      Uri.parse("$baseUrl/stops/add"),
+      headers: {"Content-Type": "application/json"},
+      body: jsonEncode(data),
+    );
+    return res.statusCode == 201;
   }
 }
